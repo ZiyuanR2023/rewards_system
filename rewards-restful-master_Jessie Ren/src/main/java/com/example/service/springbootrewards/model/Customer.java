1be5d9fe135a3880285f4a715224cbf3338c5aeb
ZiyuanR2023 <121977280+ZiyuanR2023@users.noreply.github.com>
2023-01-04 15:59:29 -0500

    Update Customer.java

--- rewards-restful-master_Jessie Ren/src/main/java/com/example/service/springbootrewards/model/Customer.java
+++ rewards-restful-master_Jessie Ren/src/main/java/com/example/service/springbootrewards/model/Customer.java
@@ -19,7 +19,7 @@ public class Customer {
 	private Integer id;
 	private String name;
 	@OneToMany(mappedBy="customer", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	private Set<MyTransaction> transactions;
+	private Set<Transaction> transactions;
 	@JsonInclude
 	@Transient
 	private Long rewardPoints;
@@ -47,10 +47,10 @@ public class Customer {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Set<MyTransaction> getTransactions() {
+	public Set<Transaction> getTransactions() {
 		return transactions;
 	}
-	public void setTransactions(Set<MyTransaction> transactions) {
+	public void setTransactions(Set<Transaction> transactions) {
 		this.transactions = transactions;
 	}
 	public Long getRewardPoints() {
