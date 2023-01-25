4dd5658a1378dccc65436c94ea355dda85d7d5e5
ZiyuanR2023 <121977280+ZiyuanR2023@users.noreply.github.com>
2023-01-25 18:20:10 -0500

    Update CustomerRepository.java

--- rewards-restful-master_Jessie Ren/src/main/java/com/example/service/springbootrewards/repository/CustomerRepository.java
+++ rewards-restful-master_Jessie Ren/src/main/java/com/example/service/springbootrewards/repository/CustomerRepository.java
@@ -7,5 +7,7 @@ import com.example.service.springbootrewards.model.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{
-
+  
+  @Query("select customer from Customer c left join Transaction t on c.transaction_id = t.transaction_id where c.customer_id = customerId and t.transaction_date between startDate and endDate")
+  Customer getThreeMonthsTransactionsById(@Param("customerId") Integer id, @Param("startDate") Date startDate, @Param("endDate") Date endDate);
 }
